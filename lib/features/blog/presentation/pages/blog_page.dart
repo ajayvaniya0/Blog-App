ba4401ec7198eafd556c81172dab780c9675a37a
Ajay <ajayvaniya54@gmail.com>
2024-08-10 11:58:31 +0530

    Blog Card UI

--- lib/features/blog/presentation/pages/blog_page.dart
+++ lib/features/blog/presentation/pages/blog_page.dart
@@ -1,7 +1,9 @@
 import 'package:blog_app/core/common/widgets/loader.dart';
+import 'package:blog_app/core/theme/app_pallete.dart';
 import 'package:blog_app/core/utils/show_snackbar.dart';
 import 'package:blog_app/features/blog/presentation/bloc/blog_bloc.dart';
 import 'package:blog_app/features/blog/presentation/pages/add_new_blog_page.dart';
+import 'package:blog_app/features/blog/presentation/widgets/blog_card.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
@@ -55,7 +57,10 @@ class _BlogPageState extends State<BlogPage> {
               itemCount: state.blogs.length,
               itemBuilder: (context, index) {
                 final blog = state.blogs[index];
-                return Text(blog.title);
+                return BlogCard(
+                  blog: blog,
+                  color: AppPallete.gradient1,
+                );
               },
             );
           }
